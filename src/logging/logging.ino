866aaa6363eb2e74a5dc58841768f2e647441f3b
AntonSangho <sanghoemail@gmail.com>
2024-07-09 10:22:15 +0900

    first commit

---
+++ src/logging/logging.ino
@@ -0,0 +1,33 @@
+#include <Adafruit_AHTX0.h>
+
+Adafruit_AHTX0 aht;
+
+int second = 0;
+
+void setup() {
+  Serial.begin(115200);
+  //Serial.println("Adafruit AHT10/AHT20 demo!");
+
+  if (! aht.begin()) {
+    Serial.println("Could not find AHT? Check wiring");
+    while (1) delay(10);
+  }
+  Serial.println("AHT10 or AHT20 found");
+}
+
+void loop() {
+  Serial.print(second);
+  Serial.print(",");
+  sensors_event_t humidity, temp;
+  aht.getEvent(&humidity, &temp);// populate temp and humidity objects with fresh data
+  //Serial.print("Temperature: "); 
+  Serial.print(temp.temperature); 
+  Serial.print(",");
+  //Serial.println(" degrees C");
+  //Serial.print("Humidity: "); 
+  Serial.print(humidity.relative_humidity); 
+  Serial.println();
+  //Serial.println("% rH");
+  second++;
+  delay(1000);
+}
